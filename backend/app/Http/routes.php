316ab66e48dafef032405b978306f923d055495b
Marty McLaughlin <martymclaugh@gmail.com>
2016-11-22 13:59:13 -0800

    set up laravel router

--- backend/app/Http/routes.php
+++ backend/app/Http/routes.php
@@ -6,11 +6,36 @@
 |--------------------------------------------------------------------------
 |
 | Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// API Routes come first
+$api = app('Dingo\Api\Routing\Router');
+$api->version('v1',function($api){
+    header('Access-Control-Allow-Origin: http://localhost:4200');
+    header('Access-Control-Allow-Headers: Origin, Content-Type, Authorization');
+    header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, PATCH, DELETE');
+
+    // API
+    $api->group(['namespace'=>'App\Http\Controllers\Api'],function($api){
+        // Auth
+        $api->post('auth/login','Auth\AuthController@postLogin');
+        $api->post('auth/token-refresh','Auth\AuthController@refreshToken');
+        $api->post('users','Auth\UsersController@store');
+
+        // Protected methods (require auth)
+        $api->group(['middleware'=>'api.auth'],function($api){
+            
+        });
+
+        // Public methods
+
+    });
 });
+
+// Catchall - Displays Ember app
+Route::any('{catchall}',function(){
+    return view('index');
+})->where('catchall', '(.*)');
