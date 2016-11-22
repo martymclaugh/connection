5db69c99a4b472bdef67084f9ecbb3c15b0903ed
Marty McLaughlin <martymclaugh@gmail.com>
2016-11-22 13:29:40 -0800

    add dingo api

--- backend/config/app.php
+++ backend/config/app.php
@@ -145,6 +145,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Custom Service Providers
+         */
+
+        Dingo\Api\Provider\LaravelServiceProvider::class,
     ],
 
     /*
