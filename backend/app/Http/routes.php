4d7e90959cfd79aa195aa5cb21711707aa3b13c8
Marty McLaughlin <martymclaugh@gmail.com>
2016-11-22 16:18:03 -0800

    fix routes

--- backend/app/Http/routes.php
+++ backend/app/Http/routes.php
@@ -31,19 +31,21 @@ $api->version('v1',function($api){
         });
 
         // Public methods
-        $api->get('/', 'App\Api\Controllers\UserController@index');
-
-        // User view displays specific user (www.rainbowconnection.com/:user_id)
-        $api->get('/{id}', 'App\Api\Controllers\UserController@show');
-
-        // Delete friend relationship (www.rainbowconnection.com/users/:friend_one_id/friends/:friend_two_id)
-        // $api->delete('App\Api\Controllers\Userusers/{friend_one_id}/friends/{friend_two_id}')
-
-        // Test Endpoint to populate database with n amount of users (www.rainbowconnection.com/:amount_of_users)
-        // post to users seed function
-        $api->post('/{amount_of_users}', 'App\Api\Controllers\UserController@seed')        
     });
 });
+
+Route::get('/', 'Users@index');
+
+// User view displays specific user (www.rainbowconnection.com/:user_id)
+Route::get('/{id}', 'Users@show');
+
+// Delete friend relationship (www.rainbowconnection.com/users/:friend_one_id/friends/:friend_two_id)
+// Route::delete('/users/{friend_one_id}/friends/{friend_two_id}');
+
+// Test Endpoint to populate database with n amount of users (www.rainbowconnection.com/:amount_of_users)
+// post to users seed function
+Route::post('/{amount_of_users}', 'Users@seed');
+
 
 // Catchall - Displays Ember app
 Route::any('{catchall}',function(){
